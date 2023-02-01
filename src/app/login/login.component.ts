a98e67fd927a1b4d9b745fe4189a9b2ccdab675b
Sooraj-RK <soorajrk007@gmail.com>
2023-02-01 22:31:24 +0530

    Third day

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -15,14 +17,8 @@ export class LoginComponent {
    
   psw=''
 
-  userDetails:any={
-    1000:{acno:1000,username:"anu",password:"abc123",balance:0},
-    1001:{acno:1001,username:"amal",password:"abc123",balance:0},
-    1002:{acno:1002,username:"arun",password:"abc123",balance:0},
-    1003:{acno:1003,username:"akil",password:"abc123",balance:0},
-    
-  }
-  constructor() { }
+  
+  constructor(private router:Router,private ds:DataService) { }
 
   ngOnInit(): void {
 
@@ -31,10 +27,11 @@ export class LoginComponent {
   login(){
     var accnum=this.acno
     var psw= this.psw
-    var userDetails=this.userDetails
+    var userDetails=this.ds.userDetails
     if(accnum in userDetails){
       if(psw==userDetails[accnum]["password"]){
         alert ("login success")
+        this.router.navigateByUrl("dashboard")
 
       }
       else{
