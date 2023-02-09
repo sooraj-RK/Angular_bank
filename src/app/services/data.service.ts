fb07ba2ef579b5caa6589f04bcbf1095294a3be5
Sooraj-RK <soorajrk007@gmail.com>
2023-02-09 16:53:17 +0530

    Nineth day

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,23 +9,53 @@ export class DataService {
 
   currentUser:any
   currentAcno:any
+  userDetails:any
 
-  constructor() { }
-  
-  userDetails:any={
-    1000:{acno:1000,username:"anu",password:"abc123",balance:0, transaction:[]},
-    1001:{acno:1001,username:"amal",password:"abc123",balance:0 , transaction:[]},
-    1002:{acno:1002,username:"arun",password:"abc123",balance:0 , transaction:[]},
-    1003:{acno:1003,username:"akil",password:"abc123",balance:0 , transaction:[]}
-    
+  constructor() { 
+    this.getData()
   }
+  
+  // userDetails:any={
+  //   1000:{acno:1000,username:"anu",password:"abc123",balance:0, transaction:[]},
+  //   1001:{acno:1001,username:"amal",password:"abc123",balance:0 , transaction:[]},
+  //   1002:{acno:1002,username:"arun",password:"abc123",balance:0 , transaction:[]},
+  //   1003:{acno:1003,username:"akil",password:"abc123",balance:0 , transaction:[]}
+    
+  // }
+
+  saveData(){
+    if(this.userDetails){
+      localStorage.setItem("database",JSON.stringify(this.userDetails))
+    }
+    if(this.currentUser){
+      localStorage.setItem("currentUser",this.currentUser)
+    }
+    if (this.currentAcno){
+      localStorage.setItem("currentAcno",JSON.stringify(this.currentAcno))
+    }
+  }
+
+  getData(){
+    if(localStorage.getItem('database')){
+      this.userDetails=JSON.parse(localStorage.getItem('database')|| "")
+    }
+    if(localStorage.getItem('currentUser')){
+      this.currentUser=localStorage.getItem('currentUser')
+    }
+    if(localStorage.getItem('currentAcno')){
+      this.currentAcno=JSON.parse(localStorage.getItem('currentAcno') || "")
+    }
+  }
+
   register(uname:any,acno:any,psw:any){
     if(acno in this.userDetails){
     return false
   }
   else{
-    this.userDetails[acno]={acno,username:uname,password:psw,balance:0}
+    this.userDetails[acno]={acno,username: uname,password: psw,balance:0,transaction: []}
     console.log(this.userDetails);
+
+    this.saveData()
     
     return true
 
@@ -41,6 +71,7 @@ login(acno:any,psw:any){
       // console.log(this.currentUser);
 
       this.currentAcno=acno
+      this.saveData()
       
     return true
     }
@@ -70,6 +101,8 @@ deposit(acnum:any,password: any ,amount:any){
 
       userDetails[acnum]["transaction"].push({Type:"CREDIT",amount:amnt})
 
+      this.saveData()
+
       
 
       return userDetails[acnum]["balance"]
@@ -97,8 +130,7 @@ withdraw(acnum:any,password: any ,amount:any){
 
       console.log(userDetails);
       
-
-      console.log(userDetails);
+      this.saveData()
 
       return userDetails[acnum]["balance"]
     }
